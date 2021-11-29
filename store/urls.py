d90976a1a7e7bf4603d9f2061af7db2b3fb4d0d3
Francisco Sabino <Francisco Sabino>
2021-11-29 22:44:56 +0100

    added index page as home page and css

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "store"
 
 urlpatterns = [
-    path("", views.product_all, name="product_all"),
+    path("", views.index, name="index"),
+    path("all_products", views.product_all, name="product_all"),
     path("<slug:slug>", views.product_detail, name="product_detail"),
     path("shop/<slug:category_slug>/", views.category_list, name="category_list"),
 ]
--- store/views.py
+++ store/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
+def index(request):
+    products = Product.products.all()
+    return render(request, "index.html", {"products": products})
+
+
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
@@ -15,7 +20,7 @@ def category_list(request, category_slug=None):
 
 def product_all(request):
     products = Product.products.all()
-    return render(request, "store/home.html", {"products": products})
+    return render(request, "store/all_products.html", {"products": products})
 
 
 def product_detail(request, slug):
