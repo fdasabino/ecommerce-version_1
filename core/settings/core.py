92734fcb918c41473d901bebbf8cae71d1bf276e
Francisco Sabino <Francisco Sabino>
2021-12-01 16:23:46 +0100

    added django_debug_toolbar

--- core/settings/core.py
+++ core/settings/core.py
@@ -17,7 +17,7 @@ if os.path.isfile("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
